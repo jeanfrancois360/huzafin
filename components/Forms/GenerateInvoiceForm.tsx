905f1379d28c863697993d39b4ff63926dcecf1e
Jean Francois Munyaneza <jeanfrancoismunyaneza@Jeans-MacBook-Pro.local>
2024-02-22 17:00:45 +0200

    ci: code refactoring

--- components/Forms/GenerateInvoiceForm.tsx
+++ components/Forms/GenerateInvoiceForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import { Formik, FieldArray } from 'formik'
 import * as Yup from 'yup';
--- components/Forms/SignInForm.tsx
+++ components/Forms/SignInForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import { Formik } from 'formik'
 import Link from 'next/link'
